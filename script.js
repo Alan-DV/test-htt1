9cf07cd0e1af2d61f9dd2c593c9eae57c7e77145
Alan <alu.dvera@gmail.com>
2026-01-08 16:23:27 -0300

    edit script para carrusel

--- script.js
+++ script.js
@@ -4,24 +4,27 @@ let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+    isDown = true;
+    slider.classList.add('dragging'); // Desactivamos el "snap"
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
 });
 
 slider.addEventListener('mouseleave', () => {
-  isDown = false;
+    isDown = false;
+    slider.classList.remove('dragging');
 });
 
 slider.addEventListener('mouseup', () => {
-  isDown = false;
+    isDown = false;
+    slider.classList.remove('dragging'); // Reactivamos el "snap" para que encaje al soltar
 });
 
 slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; // Velocidad de desplazamiento
-  slider.scrollLeft = scrollLeft - walk;
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    // Bajamos el multiplicador a 1.5 para que no sea tan veloz y se sienta natural
+    const walk = (x - startX) * 1.5; 
+    slider.scrollLeft = scrollLeft - walk;
 });
